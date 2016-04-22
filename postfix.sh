d83c121323460884d28c734c29058fef980aeee3
Bojan Čekrlić <bojan@cekrlic.com>
2016-04-22 14:23:36 +0200

    Minor tweaks -- create alias file, disable SMTPUTF8 support, which is not possible anyways

--- postfix.sh
+++ postfix.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Disable SMTPUTF8, because libraries (ICU) are missing in alpine
+postconf -e smtputf8_enable=no
+
+# Update aliases database. It's not used, but postfix complains if the .db file is missing
+postalias /etc/postfix/aliases
+
 # Disable local mail delivery
 postconf -e mydestination=
 # Don't relay for any domains
